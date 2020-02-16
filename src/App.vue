c0b2bfa0c7347032497b5f6f5fed0517acf6c45d
adrianorod <adrianoazevedo.r@gmail.com>
2020-02-16 13:03:47 -0300

    feat(app): importa component Header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <SvgSprite></SvgSprite>
+    <Header></Header>
     <router-view></router-view>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+
+import Header from '@/components/Header.vue';
 import SvgSprite from '@/components/SvgSprite.vue';
 
 @Component({
   components: {
+    Header,
     SvgSprite,
   },
 })
