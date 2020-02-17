af1b944898da12b916062a4b3a862097688e5a4a
adrianorod <adrianoazevedo.r@gmail.com>
2020-02-17 02:41:05 -0300

    feat(header): implementa form de busca

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,9 +8,12 @@
 
     <div class="actions">
       <div class="search">
-        <svg class="icon-action">
+        <svg class="icon-action" @click.prevent="handleSearchInput">
           <use xlink:href="#icon-search"></use>
         </svg>
+        <div :class="'input' + (isSearchInputVisible ? ' -is-visible' : '')">
+          <input v-model="searchValue" type="text" />
+        </div>
       </div>
 
       <div class="city">
@@ -41,11 +44,14 @@ import UF from '@/utils/constants';
 export default class Header extends Vue {
   isCitySelectVisible: boolean;
 
+  isSearchInputVisible: boolean;
+
   UFList: UFModel[];
 
   constructor() {
     super();
     this.isCitySelectVisible = false;
+    this.isSearchInputVisible = false;
     this.UFList = UF;
   }
 
@@ -53,8 +59,20 @@ export default class Header extends Vue {
     return this.$store.state.selectedUF;
   }
 
+  get searchValue() {
+    return this.$store.state.searchValue;
+  }
+
+  set searchValue(value) {
+    this.$store.commit('setSearchValue', value);
+  }
+
   handleCitySelect() {
     this.isCitySelectVisible = !this.isCitySelectVisible;
+  }
+
+  handleSearchInput() {
+    this.isSearchInputVisible = !this.isSearchInputVisible;
   }
 
   selectCity(city: UFModel) {
@@ -119,6 +137,7 @@ export default class Header extends Vue {
         position: absolute;
         top: 12px;
         right: 4px;
+        z-index: 1;
 
         &.-is-visible {
           padding: 5px 0 0;
@@ -146,6 +165,46 @@ export default class Header extends Vue {
           &:hover {
             background-color: #000;
           }
+        }
+      }
+    }
+
+    > .search {
+      position: relative;
+
+      > .input {
+        position: absolute;
+        top: 25px;
+        left: 50%;
+        max-height: 0;
+        padding: 0;
+        overflow: hidden;
+        transition: max-height .3s ease, padding .2s ease;
+        transform: translateX(-50%);
+
+        &.-is-visible {
+          padding: 8px 0 0;
+          max-height: 50px;
+        }
+
+        &:after {
+          content: '';
+          position: absolute;
+          top: 8px;
+          left: 50%;
+          background-color: #fff;
+          width: 10px;
+          height: 10px;
+          transform: rotate(45deg) translateX(-50%);
+          z-index: -1;
+        }
+
+        > input {
+          color: #000;
+          padding: 6px;
+          border-radius: 10px;
+          border: 0;
+          width: 200px;
         }
       }
     }
