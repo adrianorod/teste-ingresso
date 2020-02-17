18bb71540af873acef34ca43c83f1efdf90b8d5d
adrianorod <adrianoazevedo.r@gmail.com>
2020-02-16 23:15:55 -0300

    feat(svgsprite): adiciona icon de loading ao projeto

--- src/components/SvgSprite.vue
+++ src/components/SvgSprite.vue
@@ -21,6 +21,11 @@
       <title>search</title>
       <path fill-rule="evenodd" d="M9.75 2.75a6.625 6.625 0 0 1 4.99 10.983c.039.027.077.06.113.095l5.569 5.57c.341.341.36.875.043 1.192-.317.318-.851.298-1.193-.043l-5.569-5.57a.939.939 0 0 1-.142-.182A6.625 6.625 0 1 1 9.75 2.75zm0 1.625a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm-3.417 3.74c.206.019.36.2.341.406-.184 2.152.69 3.352 2.703 3.727a.375.375 0 0 1-.138.737c-2.398-.447-3.529-1.998-3.312-4.528.018-.206.2-.359.406-.341z"></path>
     </symbol>
+    <symbol viewBox="0 0 100 100" id="icon-loading">
+      <circle cx="50" cy="50" fill="none" stroke="#ffffff" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138" transform="rotate(70.3584 50 50)">
+        <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
+      </circle>
+    </symbol>
   </svg>
 </template>
 
