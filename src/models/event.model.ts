bf40402ac223b04d608e6063cc7e8201d7bf44ec
adrianorod <adrianoazevedo.r@gmail.com>
2020-02-16 23:16:25 -0300

    feat(movie.service, event.model): padroniza retorno do request

---
+++ src/models/event.model.ts
@@ -0,0 +1,7 @@
+import MovieModel from './movie.model';
+
+interface EventModel {
+  event: MovieModel;
+}
+
+export default EventModel;
--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,11 +1,11 @@
-import MovieModel from '@/models/movie.model';
+import EventModel from '@/models/event.model';
 import api from './api.service';
 
 export default class MovieService {
   public static getFilms(uf: number) {
     return api.get(`/${uf}/partnership/home`)
       .then((res) => res.data)
-      .then((data: MovieModel[]) => data)
+      .then((data) => data.map((item: EventModel) => item.event))
       .catch((error) => console.error(error));
   }
 }
