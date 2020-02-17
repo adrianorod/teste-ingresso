aac75a2d6b26f93ffefbc305f8fcabb87277cf0f
adrianorod <adrianoazevedo.r@gmail.com>
2020-02-17 01:56:46 -0300

    feat(home): atualiza listagem de filmes quando UF mudar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
-    <svg class="icon-loading" v-if="$store.state.movies.length === 0 && !$store.state.hasError">
+    <svg class="icon-loading" v-if="$store.state.movies.length === 0 && !hasError">
       <use xlink:href="#icon-loading"></use>
     </svg>
-    <div class="error-message" v-if="$store.state.hasError">
+    <div class="error-message" v-if="hasError">
       Ops! Parece que algo não carregou corretamente. Pode tentar novamente? :/
     </div>
 
@@ -19,7 +19,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Watch } from 'vue-property-decorator';
 import MovieService from '@/services/movie.service';
 
 @Component({
@@ -30,10 +30,28 @@ export default class Home extends Vue {
     this.getMovies();
   }
 
+  get selectedUF() {
+    return this.$store.state.selectedUF;
+  }
+
+  get hasError() {
+    return this.$store.state.hasError;
+  }
+
+  get movies() {
+    return this.$store.state.movies;
+  }
+
   async getMovies() {
-    const data = await MovieService.getFilms(this.$store.state.selectedUF.id);
-    if (data) this.$store.state.movies = data;
-    else this.$store.state.hasError = true;
+    this.$store.commit('setMovies', []);
+    const data = await MovieService.getFilms(this.selectedUF.id);
+    if (data) this.$store.commit('setMovies', data);
+    else this.$store.commit('setHasError', true);
+  }
+
+  @Watch('selectedUF')
+  onSelectedUFChanged() {
+    this.getMovies();
   }
 }
 </script>
