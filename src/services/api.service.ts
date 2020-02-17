28719a5403c4ff8c1f28f34fed221ea539e45894
adrianorod <adrianoazevedo.r@gmail.com>
2020-02-16 21:58:23 -0300

    refactor(api.service): utiliza var de ambiente nos requests

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://api-content.ingresso.com/v0/templates/highlights',
+  baseURL: `${process.env.VUE_APP_API}/v0/templates/highlights`,
 });
